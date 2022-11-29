a173cb19128236d2daf87a3f42d81cc0a20b46ac
vsandberg <sandbergville@hotmail.com>
2022-11-29 12:15:41 +0200

    datarakenteet tehty

--- kmeans/kmeans.py
+++ kmeans/kmeans.py
@@ -14,7 +14,28 @@ ax.scatter(datax,datay,dataz)
 ax.set_xlabel('x')
 ax.set_ylabel('y')
 ax.set_zlabel('z')
-plt.show()
+#plt.show()
+
+numberOfRows = len(data)/3
+numberOfRows = int(numberOfRows)
+min = np.min(data)
+max = np.max(data)
+
+random = np.random.randint(min,max,size=(4,3))
+print(random)
+
+centerPointCumulativeSum = np.zeros((4,3)) # . Tähän summataan aina voittavalle keskipisteelle yhden datapisteen x,y,z komponentit.
+print(centerPointCumulativeSum)
+
+Counts = np.zeros((1,4)) # tänne kasvatetaan aina voittavan keskipisteen datapisteiden lukumäärää yhdellä jokaisen voiton jälkeen.
+print(Counts)
+
+Distances = np.zeros((1,4)) # tähän talletetaan laskennan edetessä yksittäisen x,y,z pisteen etäisyys kaikkiin keskipisteet datarakenteessa oleviin 4 keskipisteeseen ja nuo 4 etäisyysarvoa talletetaan tähän muuttujaan.
+print(Distances)
+
+
+
+
 
 
 
