d39c2da3b0bf1219fbcb81bf706fb3dedd7caed5
vsandberg <sandbergville@hotmail.com>
2022-12-05 12:33:21 +0200

    kmeans muokkauksia

--- kmeans/kmeans.py
+++ kmeans/kmeans.py
@@ -1,33 +1,41 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from numpy import linalg as LA
-
+from mpl_toolkits import mplot3d
+import pandas as pd
 
 
 def rows(data):
-    numberOfRows = len(data)/3
+    numberOfRows = len(data)
     numberOfRows = int(numberOfRows)
     return numberOfRows
 
+def dataprocessing(numberOfRows,data):
+    datamatrix = np.zeros((numberOfRows,3))
+    
+    datamatrix[:,0] = data.values[0::,5]
+    datamatrix[:,1] = data.values[0::,6]
+    datamatrix[:,2] = data.values[0::,7]
+    
+  
+    return datamatrix
+    
+
+
+
 def randomData():
-    random = np.random.randint(min,max,size=(4,3))
+    random = np.random.randint(min,max,size=(6,3))
     return random
 
-def dataprocessing(numberOfRows):
-    datamatrix = np.zeros((numberOfRows,3))
-    datamatrix[:,0] = data[0::3]
-    datamatrix[:,1] = data[1::3]
-    datamatrix[:,2] = data[2::3]
-    return datamatrix
+
 
 def kMeans(random,datamatrix):
     for k in range(10):
-        centerPointCumulativeSum = np.zeros((4,3))
-        Counts = np.zeros(4) 
-        values = np.zeros(4)
-        avgDistance = np.zeros((4,3))
+        centerPointCumulativeSum = np.zeros((6,3))
+        Counts = np.zeros(6) 
+        values = np.zeros(6)
+        avgDistance = np.zeros((6,3))
         for i in range(numberOfRows):
-            for j in range(4):
+            for j in range(6):
                 value = np.abs(np.sqrt(np.power((random[j,0]- datamatrix[i,0]),2) + # X
                                 np.power((random[j,1]- datamatrix[i,1]),2) +        # Y
                                 np.power((random[j,2]- datamatrix[i,2]),2)))        # Z
@@ -37,55 +45,49 @@ def kMeans(random,datamatrix):
             centerPointCumulativeSum[p,0:3] += datamatrix[i,0:3]
 
             #Flag = np.min(Counts)
-        for f in range(4):
+        for f in range(6):
             if Counts[f] == 0:
                 avgDistance[f] = np.random.randint(min,max,size=3)
 
             else:
                 avgDistance[f] = (centerPointCumulativeSum[f] / Counts[f])
-                avgDistance = np.around(avgDistance,4) # kahden decimaalin tarkkuudella
+                avgDistance = np.around(avgDistance,4)
 
+        
+        random = avgDistance
         print(avgDistance, "\n")
-        random = randomData()
-
-        
-
     
-        
-            
-            
-            
-        
-        
-
-
-
-    
-if __name__=="__main__":
-    data = np.loadtxt('C:/Users/sandb/Desktop/koneoppimisenperusteet/projektiviikko5/putty.log')
-
-    datax = data[0::3]
-    datay = data[1::3]
-    dataz = data[2::3]
-
     fig = plt.figure()
     ax = fig.add_subplot(projection='3d')
-    ax.scatter(datax,datay,dataz)
+    for i in range(numberOfRows):
+        ax.scatter(datamatrix[:,0],datamatrix[:,1],datamatrix[:,2],color='g')
+    for i in range(6):
+        ax.scatter(avgDistance[:,0],avgDistance[:,1],avgDistance[:,2],marker='*',s=200,color='r')
     ax.set_xlabel('x')
     ax.set_ylabel('y')
     ax.set_zlabel('z')
-    #plt.show()
+    plt.show()
+        
+
+
+if __name__=="__main__":
+    
+    filename = "C:/Users/sandb/Desktop/koneoppimisenperusteet/projektiviikko5/file.csv"
+    data = pd.read_csv(filename, delimiter=";")
+    
+    #data = np.loadtxt('C:/Users/sandb/Desktop/koneoppimisenperusteet/projektiviikko5/putty.log')
+    
     global min
     global max
     global rounds
-    rounds = 3
-    min = np.min(data)
-    max = np.max(data)
-    random = randomData()
-    numberOfRows = rows(data)
-    datamatrix = dataprocessing(numberOfRows)
-    kMeans(random,datamatrix)
+    min = np.min(data.values[0::,5:7])
+    max = np.max(data.values[0::,5:7])
 
+    
+    numberOfRows = rows(data)
+    datamatrix = dataprocessing(numberOfRows,data)
+    random = randomData()
+    kMeans(random,datamatrix)
 
 
 
